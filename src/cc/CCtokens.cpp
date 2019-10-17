0e8613990b8478e649b2e0f224ef99e7e7c203d7
dainezasc <32322781+dainezasc@users.noreply.github.com>
2019-10-17 22:10:09 +0200

    Update CCtokens.cpp

--- src/cc/CCtokens.cpp
+++ src/cc/CCtokens.cpp
@@ -1315,8 +1315,8 @@ UniValue TokenViewUpdates(uint256 tokenid, int32_t samplenum, int recursive)
 			if (!(total < samplenum || samplenum == 0))
 				break;
 			
-			if (GetTransaction(txBaton.vin[1].prevout, txBaton, hashBlock) && !hashBlock.IsNull())
-				std::cerr << "prev txid " << txBaton.vin[1].prevout << std::endl;
+			if (GetTransaction(txBaton.vin[1].prevout.hash.GetHex(), txBaton, hashBlock) && !hashBlock.IsNull())
+				std::cerr << "prev txid " << txBaton.vin[1].prevout.hash.GetHex() << std::endl;
 			break;
 		}
 		result.push_back(Pair("result", "error"));
