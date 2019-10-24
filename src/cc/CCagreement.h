d2c8e779ccf4ec7bd30e06840dd05f573f276e23
jako4931 <jakob2665gmail.com>
2019-10-24 14:38:40 +0200

    dd

--- src/cc/CCagreement.h
+++ src/cc/CCagreement.h
@@ -8,7 +8,7 @@
 
 //bool AgreementsValidate(struct CCcontract_info* cpAgreement, Eval* eval, const CTransaction& tx, uint32_t nIn);
 std::string CreateProposal(int64_t txfee, int64_t duration, uint256 assetHash);
-UniValue InitialProposalInfo(uint256 proposalid);
-UniValue InitialProposalList();
+//UniValue InitialProposalInfo(uint256 proposalid);
+//UniValue InitialProposalList();
 
 #endif
