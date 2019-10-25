3739ec3909391f11524dcea91d0eb8269e23fbee
dainezasc <32322781+dainezasc@users.noreply.github.com>
2019-10-25 14:24:33 +0200

    Update CCtokens.cpp

--- src/cc/CCtokens.cpp
+++ src/cc/CCtokens.cpp
@@ -129,7 +129,7 @@ bool TokensValidate(struct CCcontract_info* cp, Eval* eval, const CTransaction& 
 	{
 		if((tx.vin[i].prevout.hash == tokenid && tx.vin[i].prevout.n == 2) || //in tokenid tx, baton vout is vout2
 			(eval->GetTxUnconfirmed(tx.vin[i].prevout.hash, referenceTx, hashBlock) != 0 && myGetTransaction(tx.vin[i].prevout.hash, referenceTx, hashBlock) &&
-			DecodeTokenUpdateOpRet(referenceTx.vout[referenceTx.vout.size() - 1].scriptPubKey, updaterPubkey, dummyRefTokenId) == 'u' &&
+			DecodeTokenUpdateOpRet(referenceTx.vout[referenceTx.vout.size() - 1].scriptPubKey, updaterPubkey, tokenid) == 'u' &&
 			tx.vin[i].prevout.n == 0)) //in update tx, baton vout is vout0
 			isSpendingBaton = true;
 		//std::cerr << "tx.vin[" << i << "].prevout.hash hex=" << tx.vin[i].prevout.hash.GetHex() << std::endl;
@@ -198,9 +198,9 @@ bool TokensValidate(struct CCcontract_info* cp, Eval* eval, const CTransaction& 
 			// if asset or master license: check if tokenid ownership percent > ownerperc
 			if (tokenType == "a" || tokenType == "m")
 			{
+				std::cerr << "ownership=" << GetTokenOwnershipPercent(updaterPubkey, tokenid) << " ownerperc=" << ownerPerc << std::endl;
 				if (GetTokenOwnershipPercent(updaterPubkey, tokenid) < ownerPerc)
 					return eval->Invalid("updater pubkey does not own enough tokens to update");
-				std::cerr << "ownership=" << GetTokenOwnershipPercent(updaterPubkey, tokenid) << " ownerperc=" << ownerPerc << std::endl;
 			}
 			
 			//if master or sub license: must be creator
