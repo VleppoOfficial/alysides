883edbf22fb91054f3e978512e0e415acf8462f1
dainezasc <32322781+dainezasc@users.noreply.github.com>
2019-12-03 16:56:58 +0100

    Update CCtokens.cpp

--- src/cc/CCtokens.cpp
+++ src/cc/CCtokens.cpp
@@ -552,50 +552,21 @@ int64_t IsTokensvout(bool goDeeper, bool checkPubkeys /*<--not used, always true
                             testVouts.push_back(std::make_pair(MakeTokensCC1vout(evalCode2, tx.vout[v].nValue, voutPubkeys[1]), std::string("dual-eval2-token cc1 pk[1]")));
                     }
                 }
-				
-				fprintf(stderr,"entering 'special check'\n");
 
                 //special check for tx when spending from 1of2 CC address and one of pubkeys is global CC pubkey
-                struct CCcontract_info *cpEvalCode1,CEvalCode1;
+                /*struct CCcontract_info *cpEvalCode1,CEvalCode1;
                 cpEvalCode1 = CCinit(&CEvalCode1,evalCode1);
-				
-				fprintf(stderr,"'special check' CCinit done\n");
-				
                 CPubKey pk=GetUnspendable(cpEvalCode1,0);
-				
-				fprintf(stderr,"'special check' GetUnspendable done\n");
-				
                 testVouts.push_back( std::make_pair(MakeTokensCC1of2vout(evalCode1, tx.vout[v].nValue, voutPubkeys[0], pk), std::string("dual-eval1 pegscc cc1of2 pk[0] globalccpk")) ); 
-                
-				fprintf(stderr,"'special check' testVouts.push_back( std::make_pair(MakeTokensCC1of2vout done\n");
-				
 				if (voutPubkeys.size() == 2) testVouts.push_back( std::make_pair(MakeTokensCC1of2vout(evalCode1, tx.vout[v].nValue, voutPubkeys[1], pk), std::string("dual-eval1 pegscc cc1of2 pk[1] globalccpk")) );
-                
-				fprintf(stderr,"'special check' (voutPubkeys.size() == 2) done\n");
-				
 				if (evalCode2!=0)
                 {
-					fprintf(stderr,"'special check' (evalCode2!=0) enter\n");
-					
                     struct CCcontract_info *cpEvalCode2,CEvalCode2;
                     cpEvalCode2 = CCinit(&CEvalCode2,evalCode2);
-					
-					fprintf(stderr,"'special check' CCinit done\n");
-					
                     CPubKey pk=GetUnspendable(cpEvalCode2,0);
-					
-					fprintf(stderr,"'special check' GetUnspendable done\n");
-					
                     testVouts.push_back( std::make_pair(MakeTokensCC1of2vout(evalCode2, tx.vout[v].nValue, voutPubkeys[0], pk), std::string("dual-eval2 pegscc cc1of2 pk[0] globalccpk")) ); 
-                    
-					fprintf(stderr,"'special check' testVouts.push_back( std::make_pair(MakeTokensCC1of2vout done\n");
-					
 					if (voutPubkeys.size() == 2) testVouts.push_back( std::make_pair(MakeTokensCC1of2vout(evalCode2, tx.vout[v].nValue, voutPubkeys[1], pk), std::string("dual-eval2 pegscc cc1of2 pk[1] globalccpk")) );
-					
-					fprintf(stderr,"'special check' (voutPubkeys.size() == 2) done\n");
-				}
-				
-				fprintf(stderr,"exiting 'special check'\n");
+				}*/
 
 				// maybe it is single-eval or dual/three-eval token change?
 				std::vector<CPubKey> vinPubkeys, vinPubkeysUnfiltered;
