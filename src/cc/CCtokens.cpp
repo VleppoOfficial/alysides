37e0995cd2a4a8a27d0ab505018c47f71dc5b2b0
dainezasc <32322781+dainezasc@users.noreply.github.com>
2019-12-05 19:02:42 +0100

    Update CCtokens.cpp

--- src/cc/CCtokens.cpp
+++ src/cc/CCtokens.cpp
@@ -1030,7 +1030,7 @@ int32_t GetOwnerPubkeys(uint256 txid, uint256 reftokenid, struct CCcontract_info
 		owners.push_back(std::vector<uint8_t>(voutPubkeys[1].begin(), voutPubkeys[1].end()));
 	// if searching for a specific pubkey, return early
 	if (!searchpubkey.empty() && std::find(owners.begin(), owners.end(), searchpubkey) != owners.end()) {
-		std::cerr << "GetOwnerPubkeys() found search pubkeys " << HexStr(owners.back() - 1) << HexStr(owners.back()) << std::endl;
+		std::cerr << "GetOwnerPubkeys() found search pubkeys " << HexStr(owners.rbegin() - 1) << " and " << HexStr(owners.rbegin()) << std::endl;
 		return 1;
 	}
 	// iterate through all vouts in tx
